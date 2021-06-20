5259c6841784465db48ffb03fdab1c927a2e48d5
kbalubam <kevin.baluba@hotmail.com>
2021-06-20 21:55:39 +0200

    derniere version

--- public/js/dark_light.js
+++ public/js/dark_light.js
@@ -2,13 +2,16 @@ let buttonDar = document.querySelector(".dark");
 let buttonLight=buttonDar.nextElementSibling;
 let ul =Array.from(document.querySelector(".liste").querySelectorAll("a"));
 let titre=document.querySelector("h1");
-
+let titreShopping=document.querySelector(".titredl");
+let titreCarousseul=document.querySelector("#titre");
 
 let header = document.querySelector("header");
 
 buttonDar.addEventListener("click", (e)=>{
     e.preventDefault()
     document.body.style.backgroundColor="black";
+    titreShopping.style.color="white";
+    titreCarousseul.style.color="white";
     
     header.style.backgroundColor="black";
     ul.forEach(ele=>{
@@ -19,7 +22,8 @@ buttonDar.addEventListener("click", (e)=>{
 buttonLight.addEventListener("click", (e)=>{
     e.preventDefault()
     document.body.style.backgroundColor="white";
-    
+    titreShopping.style.color="black";
+    titreCarousseul.style.color="black";
     header.style.backgroundColor="white";
     ul.forEach(ele=>{
         ele.style.color="black";
--- public/js/opacity.js
+++ public/js/opacity.js
@@ -1,17 +1,22 @@
-let view = Array.from(document.querySelectorAll(".view"));
-let product= Array.from(document.querySelectorAll(".img1"));
-console.log(product);
-let glob=document.querySelector(".productGlob");
+// let view = Array.from(document.querySelectorAll(".view"));
+// let product= Array.from(document.querySelectorAll(".img1"));
+// console.log(product);
+// let glob=document.querySelector(".productGlob");
 
 
 
 
-glob.addEventListener("mouseover", (e)=>{
-    product.forEach(elem=>{
-        if(e.target==elem){
-            view[product.indexOf(elem)].setAttribute("style", "display :flex");
-
+// glob.addEventListener("mouseover", (e)=>{
+//     product.forEach(elem=>{
+//         if(e.target==elem){
+//             view[product.indexOf(elem)].setAttribute("style", "display :flex");
             
-        }
-    })
-})
+//         }
+//     })
+// })
+
+// product.forEach(elem=>{
+//     elem.addEventListener("mouseover", ()=>{
+//     view[product.indexOf(elem)].setAttribute("style", "display :flex");
+//     })
+// })
